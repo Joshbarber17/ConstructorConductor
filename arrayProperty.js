f070379ab2dc4a63648e54b60f2e8afa8789dd93
Josh Barber <Joshbarber17@me.com>
2016-05-16 12:47:57 -0600

    all done with constructor conductor

--- arrayProperty.js
+++ arrayProperty.js
@@ -1,5 +1,17 @@
-//Just like you can add methods to your own constructor, you can also add methods and properties to built in classes in JavaScript like Arrays and Objects.
+//Just like you can add methods to your own constructor, you can also add
+//methods and properties to built in classes in JavaScript like Arrays and Objects.
 
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
-  
-  //code here
+//
+// String.prototype.reverse = function(word) {
+//   return word.split('').reverse().join('');
+// };
+
+
+String.prototype.reverse = function reverse(){
+    var newWord = "";
+    for (var i = this.length - 1; i >= 0; i--) {
+      newWord += this.charAt([i]);
+    }
+    return newWord;
+  };
